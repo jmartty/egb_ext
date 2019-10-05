d6946bb874399077c2013101f96f4faa5225edba
Juan Martty <jmartty@hotmail.com>
2019-10-05 07:37:19 -0300

    Add percentages to mybets, change interval to 2s

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 window.setInterval(function() {
-  var bets = document.getElementsByClassName('bet-rate')
+  var bets = document.querySelectorAll('.bet-rate,.table-my-bets__col-2')
   for(var i = 0;i < bets.length;i++) {
     var num = bets[i].textContent.split(' ')[0]
     var perc = Math.round(100/parseFloat(bets[i].textContent))
     bets[i].textContent = num + ' ' + perc + '%'
   }
-}, 1000);
+}, 2000);
